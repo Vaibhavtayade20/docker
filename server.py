4088133ffc523428050e854b5cc9b3a1342726d1
Vaibhavtayade20 <vaibhavtayade786@gmail.com>
2023-07-26 12:56:09 +0530

    flask app v1

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+# create flask app
+app = Flask(__name__)
+
+
+# add all the routes
+
+@app.route("/", methods=["GET"])
+def root():
+    return "welcome to python flask app"
+
+
+@app.route("/name", methods=["GET"])
+def name():
+    return "Vaibhav"
+
+# run the application
+app.run(host="0.0.0.0", port=9000, debug=True)
+
